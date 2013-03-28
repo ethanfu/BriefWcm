9a534acb02920b254875ade52bc29c7cef13755c
fuxueliang <fxl545826@gmail.com>
2013-03-28 22:47:25 +0800

    first init ruby project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BriefWcm::Application.config.secret_key_base = '2c302a2d2bcb69b832175319419cc3ee8c2b4d191f47e3ab6455b78f60f63cc11e891c5097e88a44c60c992a8852d45de5109f0e621863275061b9db818cc8b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BriefWcm::Application.config.session_store :encrypted_cookie_store, key: '_BriefWcm_session'
